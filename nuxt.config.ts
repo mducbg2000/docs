ba4350a39ccca29df1190a9d81ef6216cfd169e8
Nguyễn Minh Đức <mducbg2000@gmail.com>
2025-10-28 09:29:46 +0700

    feat: init project

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { defineNuxtConfig } from "nuxt/config";
+
+export default defineNuxtConfig({
+  extends: ["docus"],
+  modules: [
+    "@nuxtjs/i18n",
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/image",
+    "@nuxt/ui",
+  ],
+  site: {
+    name: "DucNM Docs",
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      {
+        code: "en",
+        name: "English",
+      },
+      {
+        code: "vi",
+        name: "Tiếng Việt",
+      },
+    ],
+  },
+});
